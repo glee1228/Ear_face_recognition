3bcace1517c902310961653340683a592b6bcfb6
glee1228 <glee1228@users.noreply.github.com>
2018-10-23 01:07:48 +0900

    ear_feature_extraction

--- face_ear/test/ear_segment.py
+++ face_ear/test/ear_segment.py
@@ -13,16 +13,18 @@ def imgSegment(img):
 def UmaxFind(edges):
     Umaxflag =False
     rows,cols = edges.shape
-    for i in range(rows):
-        for j in range(cols):
+    print("엣지: y   x")
+    print(rows,cols)
+    for y in range(rows):
+        for x in range(cols):
             if Umaxflag==False:
-                k = edges[i,j]
+                k = edges[y,x]
                 if k==255:
                     Umaxflag=True
-                    Umax = (i,j) # Umax 열,행 좌표
+                    Umax = (y,x) # Umax 행,열 좌표
                     return Umax
 
-def Draw(img):
+def DrawOrigin(img):
     crop = img[1:119, 1:90]
     height, width = crop.shape[:2]
     blur = cv2.GaussianBlur(crop,(5,5),0)
@@ -33,19 +35,23 @@ def Draw(img):
     plt.xticks([]), plt.yticks([])
     plt.show()
 
+def Draw(img):
+    plt.imshow(img)
+    plt.show()
+
 #input이 튜플 과 x좌표,y좌표일 때
 def Euclidean1(A,x2,y2):
-    x1=A[0]
-    y1=A[1]
+    x1=A[1]
+    y1=A[0]
     #print("x1 : {0}, y1 : {1}, x2 : {2}, y2 : {3}".format(x1,y1,x2,y2))
     return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
 
 #input이 튜플과 튜플일 때
 def Euclidean2(A,B):
-    x1=A[0]
-    y1=A[1]
-    x2=B[0]
-    y2=B[1]
+    x1=A[1]
+    y1=A[0]
+    x2=B[1]
+    y2=B[0]
     #print("x1 : {0}, y1 : {1}, x2 : {2}, y2 : {3}".format(x1,y1,x2,y2))
     return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
 
@@ -71,44 +77,46 @@ def LmaxFind(edges,Umax):
 def UlbFind(edges,Umax,T):
     Ulbflag=False
     rows,cols =edges.shape
-    for j in range(cols):
-        for i in range(rows):
-            k=edges[i,j]
-            if Ulbflag==False:
-                if k==255:
-                    dist=Euclidean1(Umax,i,j)
-                    if (T-1)< dist and dist<(T+1):
-                        Ulbflag=True
-                        Ulb = (i,j) # Umax 행(x),열(y) 좌표
-                        return Ulb
+    for x in range(cols):
+        for y in range(rows):
+            k=edges[y,x]
+            dist=Euclidean1(Umax,x,y)
+            if Ulbflag==False and (T-1)< dist and dist<(T+1) and k==255:
+                Ulbflag=True
+                Ulb = (y,x) # Umax 행(y),열(x) 좌표
+                return Ulb
+            else :
+                continue
+
 #Urb는 오른쪽 위에서 아래로 향하면서 검사
 def UrbFind(edges,Umax,T):
     Urbflag = False
     rows,cols = edges.shape
-    for j in range(cols):
-        for i in range(rows):
-            k=edges[i,cols-j-1]
-            if Urbflag==False:
-                if k==255:
-                    dist=Euclidean1(Umax,i,cols-j-1)
-                    if (T-1)< dist and dist<(T+1):
-                        Urbflag=True
-                        Urb=(i,cols-j-1)
-                        return Urb
+    for x in range(cols):
+        for y in range(rows):
+            k=edges[y,cols-x-1]
+            dist=Euclidean1(Umax,cols-x-1,y)
+            if Urbflag==False and (T-1)< dist and dist<(T+1) and k==255:
+                Urbflag=True
+                Urb=(y,cols-x-1)
+                return Urb
+
+
+
 #Llb를 찾을 떄 귀의 기울어짐을 고려하여 이미지 가장 아래에서 부터 위로 보았을 때 1/3지점까지만 검사
 #Llb는 왼쪽아래에서 위로 향하면서 검사
 def LlbFind(edges,Lmax,T):
     Llbflag=False
     rows,cols =edges.shape
-    for j in range(cols):
-        for i in range(rows):
-            k=edges[rows-i-1,j]
-            if Llbflag==False:
-                if k==255:
-                    if int(Euclidean1(Lmax,rows-i-1,j))==T:
-                        Llbflag=True
-                        Llb=(rows-i-1,j)
-                        return Llb
+    for x in range(cols):
+        for y in range(rows):
+            k=edges[rows-y-1,x]
+            dist=Euclidean1(Lmax,x,rows-y-1)
+            if Llbflag==False and (T-1)< dist and dist<(T+1) and k==255:
+                Llbflag=True
+                Llb=(rows-y-1,x)
+                return Llb
+
 
 
 
@@ -117,18 +125,23 @@ def main():
     edges=imgSegment(img)
     Umax=UmaxFind(edges)
     Lmax=LmaxFind(edges,Umax)
+    print("Umax: y    x")
     print(Umax)
+    print("Lmax: y    x")
     print(Lmax)
-    T = 5
+    
+    T = 10
     
     Ulb = UlbFind(edges,Umax,T)
-    #Urb = UrbFind(edges,Umax,T)
-    #Llb = LlbFind(edges,Lmax,T)
+    print("Ulb:  y    x")
     print(Ulb)
-    #print(Urb)
-    #print(Llb)
+    Urb = UrbFind(edges,Umax,T)
+    print("Urb:  y    x")
+    print(Urb)
     
-    
-    Draw(img)
+    Llb = LlbFind(edges,Lmax,T)
+    print("Llb:  y    x")
+    print(Llb)
+    DrawOrigin(img)
 
 main()
